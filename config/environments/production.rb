a0be8955cc7cd8a468fbfc6a8e693e9d3895dd9c
armentamj <armentamj@gmail.com>
2026-03-26 18:19:42 +0100

    one small change for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "evaarmentaart.de" }
+  config.action_mailer.default_url_options = { host: "www.evaarmentaart.de" }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
